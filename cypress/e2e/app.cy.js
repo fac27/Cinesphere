1922b75ee7a88e0c4cd27158211c43e5e6b7bdf2
Zakaria Yaris <info@zakariya.me>
2023-07-19 16:47:45 +0100

    configure cypress

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,9 @@
+describe('Navigation', () => {
+  it('should check homepage', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    // The new page should contain an h1 with "About page"
+    cy.get('div').contains('Home');
+  });
+});
