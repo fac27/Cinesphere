072072717692d73cf5820db846133cd0889aa11d
eliazzo <bethazz@hotmail.co.uk>
2023-07-20 14:58:53 +0100

    get film data from TMDB

---
+++ Utils/getFilmData.ts
@@ -0,0 +1,17 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTY3NGQ3ZjE0MDIxMjFkMjMwZDcwMGE3OGM1MTRhNCIsInN1YiI6IjY0YjkxMzgzMjdkYjYxMDBlMjkxYTBlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UgU_Jv7qcmzuH3FgCHSC9azGce4OrH1Z_05DsZ5imJU'
+  }
+};
+
+const imdb_id_elemental = "tt15789038"
+
+const data = fetch(`https://api.themoviedb.org/3/find/${imdb_id_elemental}?external_source=imdb_id`, options)
+.then(response => response.json())
+.then(response => console.log(response.movie_results[0].overview))
+.catch(err => console.error(err));
+
+
+
