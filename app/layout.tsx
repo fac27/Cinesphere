b7451bf9506faaeb7d7df9a827ad1cd13c16cd93
Taha <tahafhassan@gmail.com>
2023-07-21 15:05:40 +0100

    fix lint error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import "./globals.css";
+import React from "react";
 import type { Metadata } from "next";
 import { IBM_Plex_Mono } from "next/font/google";
 
 const ibmPlexMono = IBM_Plex_Mono({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Cinesphere',
-  description: 'Find local independent film',
-}
+  title: "Cinesphere",
+  description: "Find local independent film",
+};
 
 export default function RootLayout({
   children,
