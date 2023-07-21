c45e4a6dce27b5902b8f72a13e87e1c3c114a097
eliazzo <bethazz@hotmail.co.uk>
2023-07-21 15:46:19 +0100

    burger menu - almost complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={ibmPlexMono.className}>
-        <Header/>
+        <Header />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
