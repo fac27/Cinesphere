e951df1a41e5bb923c7fb77903697d2b807dcaad
Zakaria Yaris <info@zakariya.me>
2023-07-24 12:39:20 +0100

    landing page done with test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Link from "next/link";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <>
       <Header />
+
       <div className="mt-10 mr-auto ml-auto w-80 bg-bgColour2 p-4 rounded-2xl border border-black">
         <h1 className="text-2xl font-bold">Cinesphere</h1>
         <p className="mt-5">
@@ -19,16 +21,33 @@ const Home = () => {
 
         <div className="mt-6 flex gap-10">
           <Link href={"/films"}>
-            <div className="bg-[url(https://images.pexels.com/photos/3056056/pexels-photo-3056056.jpeg?cs=srgb&dl=pexels-collis-3056056.jpg&fm=jpg)] bg-cover w-32 h-32 rounded relative">
-              <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 rounded-2xl" />
+            <div className="relative w-32 h-32">
+              <Image
+                className="rounded"
+                src="/assets/home-images/films.jpg"
+                alt={`a photo of vintage film poster`}
+                layout="fill"
+                objectFit="cover"
+              />
+
+              <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 rounded" />
               <p className="relative text-white text-xl text-center pt-10">
                 Films
               </p>
             </div>
           </Link>
-          <Link href={"/cinemas"}>
-            <div className="bg-[url(https://i2-prod.dailyrecord.co.uk/incoming/article18847157.ece/ALTERNATES/s1200b/2_IMG_1823.jpg)] bg-cover w-32 h-32 rounded relative">
-              <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 rounded-2xl" />
+
+          <Link href={"/films"}>
+            <div className="relative w-32 h-32">
+              <Image
+                className="rounded"
+                src="/assets/home-images/cinemas.webp"
+                alt={`a photo of inside cinema theater`}
+                layout="fill"
+                objectFit="cover"
+              />
+
+              <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 rounded" />
               <p className="relative text-white text-xl text-center pt-10">
                 Cinemas
               </p>
