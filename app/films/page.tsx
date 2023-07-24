2806e3a7279023cc83a5bd2e958658031c036308
eliazzo <bethazz@hotmail.co.uk>
2023-07-24 11:32:54 +0100

    add search and filter to films

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -1,11 +1,27 @@
 import FilmCard from "./components/FilmCard";
+import { BiSliderAlt } from "react-icons/bi";
 
 const Films = () => {
   return (
-  <>
-  <FilmCard />
-  </>
-  )
+    <>
+    <div className="flex flex-row justify-center mt-4">
+      <input
+        type="text"
+        className="p-2 w-40 flex border border-black rounded-lg"
+        placeholder="search"
+      ></input>
+      <button
+        type="button"
+        className="p-2 flex items-center gap-1 rounded-lg border border-black ml-3"
+      >
+        <BiSliderAlt />
+        Filter
+      </button>
+    </div>
+
+      <FilmCard />
+    </>
+  );
 };
 
 export default Films;
