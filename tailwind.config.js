ab6f08bad321e9ad2b69e47985b7049171d2a46a
simonryrie <simonryrie@gmail.com>
2023-07-24 15:10:28 +0100

    Tailwind config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'xs': '376px',
-      'sm': '576px',
-      'md': '960px',
-      'lg': '1440px',
+      xs: "376px",
+      sm: "576px",
+      md: "960px",
+      lg: "1440px",
     },
     extend: {
       backgroundImage: {
