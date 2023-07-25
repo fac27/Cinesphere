6de051ec3c03180b5a225e5013b69b9b0f213b4b
Taha <tahafhassan@gmail.com>
2023-07-25 16:37:35 +0100

    set up contact page
    body element takes up full screen

---
+++ app/contact/components/ContactForm.tsx
@@ -0,0 +1,11 @@
+const ContactForm = () => {
+  return (
+    <div className="">
+      <form className="">
+        <h1>Contact Us</h1>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import ContactForm from "./components/ContactForm";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-full">
+      <ContactForm />
+    </div>
+  );
+};
+
+export default Page;
