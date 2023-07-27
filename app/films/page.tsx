9af1ec082f633c23193d7b57ed1c5872b53f5d46
Taha <tahafhassan@gmail.com>
2023-07-27 10:51:34 +0100

    make FilterBar component

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+import { FilterBar } from "./../components/FilterBar";
+("use client");
 import { BiSliderAlt } from "react-icons/bi";
 import { useState } from "react";
 import FilmsContainer from "./components/FilmsContainer";
@@ -9,23 +10,7 @@ const Films = () => {
   return (
     <>
       <Modal isVisible={isVisible} setIsVisible={setIsVisible} />
-      <div className="flex flex-row justify-center mt-4">
-        <input
-          type="text"
-          className="p-2 w-40 flex border border-black rounded-lg"
-          placeholder="search"
-        ></input>
-        <button
-          onClick={() => {
-            setIsVisible((prev) => !prev);
-          }}
-          type="button"
-          className="p-2 flex items-center gap-1 rounded-lg border border-black ml-3"
-        >
-          <BiSliderAlt />
-          Filter
-        </button>
-      </div>
+      <FilterBar setIsVisible={setIsVisible} />
       <FilmsContainer />
     </>
   );
