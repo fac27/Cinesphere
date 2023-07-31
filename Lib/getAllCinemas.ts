c70e234da8a713c2db4442e4418d581d3f959f58
Taha <tahafhassan@gmail.com>
2023-07-31 14:57:43 +0100

    empty commit to trigger redeploy

--- Lib/getAllCinemas.ts
+++ Lib/getAllCinemas.ts
@@ -2,15 +2,13 @@
 import supabaseClient from "./supabaseClient";
 
 const getAllCinemas = async () => {
-  const { data, error } = await supabaseClient
-  .from('venue')
-  .select("*")
+  const { data, error } = await supabaseClient.from("venue").select("*");
   if (error) {
-    console.log(`Error getting cinemas: ${error.message}`);
+    console.log(`Error getting cinema data: ${error.message}`);
     throw error;
   }
-console.log(data)
-return data
-}
+  console.log(data);
+  return data;
+};
 
 export default getAllCinemas;
