47e9af61a9913c822e6a85f6509c9a709b9c29dd
eliazzo <bethazz@hotmail.co.uk>
2023-07-31 15:40:07 +0100

    select-all-screenings function

--- app/cinemas/page.tsx
+++ app/cinemas/page.tsx
@@ -4,7 +4,6 @@ import { CinemaType } from "@/Types/Object-Interfaces";
 
 const Page = async () => {
   const cinemas: CinemaType[] = await getAllCinemas();
-  console.log("page: ", cinemas);
   return <CinemaPage cinemas={cinemas} />;
 };
 
