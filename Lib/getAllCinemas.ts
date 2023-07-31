7fcea07d700229b91cef89a402d3377a840e4d96
eliazzo <bethazz@hotmail.co.uk>
2023-07-31 12:55:42 +0100

    return cinema data from database

--- Lib/getAllCinemas.ts
+++ Lib/getAllCinemas.ts
@@ -1,16 +1,16 @@
-import { CinemaType } from "@/Types/Object-Interfaces";
+// import { CinemaType } from "@/Types/Object-Interfaces";
 import supabaseClient from "./supabaseClient";
 
-const getAllCinemas = async (): Promise<CinemaType[]> => {
-  let { data, error } = await supabaseClient.from("venue").select("*");
-
+const getAllCinemas = async () => {
+  const { data, error } = await supabaseClient
+  .from('venue')
+  .select("*")
   if (error) {
     console.log(`Error getting cinemas: ${error.message}`);
     throw error;
   }
-
-  console.log("Data :", data);
-  return data;
-};
+console.log(data)
+return data
+}
 
 export default getAllCinemas;
