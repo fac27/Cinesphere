3800219441cc3c5beeeed4ae2503a84e657c6dda
Zakaria Yaris <info@zakariya.me>
2023-08-01 16:35:58 +0100

    this should fix deployment issue (hopefully)

--- app/cinemas/components/CinemaContainer.tsx
+++ app/cinemas/components/CinemaContainer.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { CinemaType } from "@/Types/Object-Interfaces";
 import CinemaCard from "./CinemaCard";
 import { useFilters } from "@/app/Context/store";
-// import Map from "@/app/components/Map";
+import Map from "@/app/components/Map";
 
 interface Props {
   distances: { cinema: string; distance: string }[];
@@ -38,7 +40,7 @@ const CinemaContainer = ({ distances, cinemas }: Props) => {
 
   return (
     <>
-      {/* <Map cinemas={filteredCinemas} /> */}
+      <Map cinemas={filteredCinemas} />
       {filteredCinemas.map((cinema) => (
         <div key={cinema.cinemaName} className="flex flex-col items-center">
           <CinemaCard cinema={cinema} distances={distances} />
